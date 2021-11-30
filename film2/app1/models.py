fa4075b501c3111c87907b3cb6fd838e6fcd9d37
Maria Aurellia <mariaadannies26@gmail.com>
2021-11-30 23:39:41 +0700

    Form

---
+++ film2/app1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class SFilm(models.Model):
+    judul = models.CharField(max_length=100)
+    poster = models.CharField(max_length=100)
+    trailer = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    tahun_rilis = models.DateTimeField()
+
+    def __str__(self):
+        return self.judul
