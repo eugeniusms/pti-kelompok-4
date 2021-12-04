0279f523b9c5b76ee4af65eb7583bdcb0c41de30
Eugenius Mario S <eugenius.mario@ui.ac.id>
2021-12-04 21:19:17 +0700

    Add files via upload

---
+++ app1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import SFilm
+
+# Register your models here.
+admin.site.register(SFilm)
---
+++ app1/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django import forms
+from .models import SFilm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+
+
+class FilmForm(forms.Form):
+    judul = forms.CharField(label='Judul Film ')
+    poster = forms.ImageField(label='Poster Film ')
+    trailer = forms.CharField(label='Link Trailer ')
+    genre = forms.CharField(label='Genre Film ')
+    tahun_rilis = forms.DateField() 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            'judul', 
+            'poster', 
+            'trailer',
+            'genre',
+            'tahun_rilis',
+            Submit('submit', 'Submit', css_class="btn-success")
+
+        )
+
+class SFilmForm(forms.ModelForm):
+    class Meta:
+        model = SFilm
+        fields = ('judul', 'poster', 'trailer', 'genre', 'tahun_rilis')
