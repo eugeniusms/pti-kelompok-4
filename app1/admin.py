72805828cd257298a96827e1898013b3d659e5bc
Kevin <kevingreg88@gmail.com>
2021-12-05 18:05:59 +0700

    try fixing migrations

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SFilm
+from .models import SFilm, Movie
 
 # Register your models here.
 admin.site.register(SFilm)
+admin.site.register(Movie)
--- app1/models.py
+++ app1/models.py
@@ -11,12 +11,12 @@ class SFilm(models.Model):
     def __str__(self):
         return self.judul
 
-# class Movie(models.Model):
-#     judul = models.CharField(max_length=100)
-#     poster = models.CharField(max_length=100)
-#     trailer = models.CharField(max_length=100)
-#     genre = models.CharField(max_length=100)
-#     tahun_rilis = models.DateTimeField()
+class Movie(models.Model):
+    judul = models.CharField(max_length=100)
+    poster = models.CharField(max_length=100)
+    trailer = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    tahun_rilis = models.DateTimeField()
 
-#     def __str__(self):
-#         return self.judul
+    def __str__(self):
+        return self.judulss
