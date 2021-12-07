4e55bf73c9fee637de7adf7e9b243bf835903f53
Eugenius Mario S <eugeniusms@gmail.com>
2021-12-07 22:52:22 +0700

    Update Kevin Serializer Film

--- app1/views.py
+++ app1/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import render
 from .forms import FilmForm, SFilmForm
 from django.http import HttpRequest
 from .models import SFilm
+from .serializers import SortSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import SortSerializer
-from rest_framework import serializers, status
+from rest_framework import status
 
 # Create your views here.
-def AddFilm(request):
-    if request.method == 'POST':
-        form = FilmForm(request.POST)
-        if form.is_valid():
-
-            judul = request.POST.get('Judul Film ')
-            genre = request.POST.get('Genre Film ')
-
-            print(judul, genre)
-
-    form = FilmForm()
-    return render(request, 'app1/form.html', {'form' : form})
+class AddFilm(APIView):
+    def post(self, request):
+        film = SFilm.objects.create(
+            judul = request.data['judul'] ,
+            poster = request.data['poster'], 
+            trailer = request.data['trailer'] ,
+            genre = request.data['genre'],
+            tahun_rilis = request.data['tahun_rilis'] ,
+            likes = request.data['likes'] ,
+            dislikes = request.data['dislikes']
+        
+        
+        )
+        serializers = SortSerializer(film, many = True)
+        return Response(serializers.data, status= status.HTTP_201_CREATED)
 
 
 def SFilm_detail(request):
